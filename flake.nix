796e0298248c6276f2165d5a5a6c390b7d918cd7
Drazape <Drazape@outlook.com>
2026-07-24 12:13:22 +0530

    feat: add metadata

--- flake.nix
+++ flake.nix
@@ -14,24 +14,31 @@
 		flake-parts.lib.mkFlake { inherit inputs; } {
 			systems = ["x86_64-linux"];
 			perSystem = { config, self', inputs', pkgs, system, ... }: {
-				packages = {
-					default = self'.packages.gotohp;
-					gotohp = pkgs.stdenv.mkDerivation {
-						name = "gotohp";
-						src = inputs.gotohp;
-						
+				packages = let pkgName = "gotohp"; in {
+					default = self'.packages.${pkgName};
+					${pkgName} = pkgs.stdenv.mkDerivation {
+						name = pkgName;
+						src = inputs.${pkgName};
+
 						nativeBuildInputs = with pkgs; [ dpkg autoPatchelfHook ];
 						buildInputs = with pkgs; [ libX11 webkitgtk_6_0 gtk4 libsoup_3 glib ];
-						
+
 						unpackPhase = ''
 								dpkg --extract -- $src ./tree/
 								cd ./tree/usr/
 						'';
-						
+
 						installPhase = ''
 								mkdir -p $out/
 								mv ./local/bin/ ./share/ $out/
 						'';
+
+						meta = {
+							description = "Unofficial Google Photos Desktop GUI Client";
+							homepage = "https://github.com/xob0t/"+pkgName;
+							license = pkgs.lib.licenses.mit;
+							mainProgram = pkgName;
+						};
 					};
 				};
 			};
